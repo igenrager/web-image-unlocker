f23bfee27c9672a11005ec806c49a8a2c3ab761f
igenrager <130927121+igenrager@users.noreply.github.com>
2025-05-23 23:40:17 +0200

    Reset

--- popup.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('smart').onclick = () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, t => {
-    chrome.scripting.executeScript({
-      target: { tabId: t[0].id },
-      files: ['smart_fallback_capture.js']
-    });
-  });
-};
